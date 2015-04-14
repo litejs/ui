ef36ba080aa307c5c3761092fdfeef8600a4b647
Lauri Rooden <lauri@rooden.ee>
2015-04-15 02:10:14 +0300

    Use space as separator in View.def

--- src/view.js
+++ src/view.js
@@ -140,7 +140,7 @@
 	}
 
 	View.def = function(str) {
-		for (var match, re = /(\S+)\?(\S+)/g; match = re.exec(str);) {
+		for (var match, re = /(\S+) (\S+)/g; match = re.exec(str);) {
 			match[2].split(",").map(function(route) {
 				View(route).file = match[1]
 			})
