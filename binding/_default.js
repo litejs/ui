1bd351da4997161f1b2bfea6fbe3d6d444c27418
Lauri Rooden <lauri@rooden.ee>
2015-05-10 20:53:26 +0300

    Update libs

--- binding/_default.js
+++ binding/_default.js
@@ -3,42 +3,42 @@
 	var hasOwn = Object.prototype.hasOwnProperty
 	, slice = Array.prototype.slice
 
-	bindingFn.once = bindingFn.raw =
+	bindingFn.once =
 	bindingWith.once =
 	bindingOn.once =
 	emitForm.once =
 	true
 
 	bindings.fn = bindingFn
-	function bindingFn(node, data, fn) {
-		fn.apply(node, slice.call(arguments, 3))
+	function bindingFn(data, fn) {
+		fn.apply(this, slice.call(arguments, 3))
 	}
 
 	bindings["if"] = bindingIf
-	function bindingIf(node, data, enabled) {
-		if (!enabled) return node.kill()
+	function bindingIf(data, enabled) {
+		if (!enabled) return this.kill()
 	}
 
 	bindings.on = bindingOn
-	function bindingOn(node, data, ev, fn, a1, a2) {
+	function bindingOn(data, ev, fn, a1, a2) {
 		if (typeof fn == "string") {
 			var _fn = fn
 			fn = function(e) {
-				Mediator.emit(_fn, e, a1, a2)
+				Mediator.emit(_fn, e, this, a1, a2)
 			}
 		}
-		node.on(ev, fn)
+		this.on(ev, fn)
 	}
 
 	bindings.with = bindingWith
-	function bindingWith(node, data, map) {
-		Object.merge(El.getScope(node, true), map)
-		return node.render()
+	function bindingWith(data, map) {
+		Object.merge(El.getScope(this, true), map)
+		return this.render()
 	}
 
 	bindings.emitForm = emitForm
-	function emitForm(node, data, ev, a1, a2) {
-		node.on("submit", function(e) {
+	function emitForm(data, ev, a1, a2) {
+		this.on("submit", function(e) {
 			var data = JSON.serializeForm(this)
 			Mediator.emit(ev, e, data, a1, a2)
 		})
