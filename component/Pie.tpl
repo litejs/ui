ad6142bc94197c3c0ddad022e09322ac6d312bac
Lauri Rooden <lauri@rooden.ee>
2016-10-26 00:12:08 +0300

    Improve svg example

---
+++ component/Pie.tpl
@@ -0,0 +1,89 @@
+
+@css
+	.Pie {
+		overflow: visible;
+		fill: currentColor;
+	}
+	.arc {
+		stroke: #7F3F98;
+		stroke-width: 5px;
+		fill: none;
+		transition: all .2s;
+	}
+	.arc+.arc                     { stroke: #F36E21; }
+	.arc+.arc+.arc                { stroke: #A6AEAE; }
+	.arc+.arc+.arc+.arc           { stroke: red; }
+	.arc+.arc+.arc+.arc+.arc      { stroke: green; }
+	.arc+.arc+.arc+.arc+.arc+.arc { stroke: blue; }
+	.arc:hover {
+		stroke-width: 8px;
+		filter: drop-shadow(2px 2px 2px rgba(0,0,0,.6));
+	}
+	.labels.x-labels {
+		text-anchor: middle;
+	}
+	.labels.y-labels {
+		text-anchor: end;
+	}
+	.grid {
+		stroke: #ccc;
+		stroke-dasharray: 0;
+		stroke-width: 1;
+	}
+	.labels {
+		font-size: 13px;
+	}
+	.label-title {
+		font-weight: bold;
+		text-transform: uppercase;
+		font-size: 12px;
+		fill: black;
+	}
+	.Chart {
+		overflow: visible;
+	}
+	.Chart-line {
+		fill: none;
+		stroke: currentColor;
+		stroke-width: 2;
+	}
+	.Chart-line > circle {
+		fill: currentColor;
+	}
+	.Chart-line.is-active {
+		stroke-width: 3;
+	}
+	.data {
+		fill: red;
+		stroke-width: 1;
+	}
+
+@el Pie
+	svg.Pie[viewBox="0 0 60 60"]
+
+
+
+@el Chart
+	svg.Chart[viewBox="0 0 600 300"][preserveAspectRatio="xMinYMid meet"]
+		&initChart
+
+@el Chart-line
+	g.Chart-line
+		&svgToLast
+		path &svgLine: points, {radius: radius}
+
+@el Chart-axes
+	g.labels.x-labels
+		text[x="100"][y="400"] 2008
+		text[x="246"][y="400"] 2009
+		text[x="392"][y="400"] 2010
+		text[x="538"][y="400"] 2011
+		text[x="684"][y="400"] 2012
+		text[x="400"][y="440"].label-title Year
+	g.grid.x-grid
+		line[x1="90"][x2="90"][y1="5"][y2="371"]
+	g.grid.y-grid
+		line[x1="90"][x2="705"][y1="370"][y2="370"]
+
+
+
--- svg.js
+++ svg.js
@@ -12,9 +12,11 @@
 	"svg circle clipPath defs g path rect text stop use linearGradient".replace(/\w+/g, c)
 	//http://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
 	function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-		var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0
-		return { x: centerX + (radius * Math.cos(angleInRadians)),
-		   y: centerY + (radius * Math.sin(angleInRadians)) }
+		var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0
+		return {
+			x: centerX + (radius * Math.cos(angleInRadians)),
+			y: centerY + (radius * Math.sin(angleInRadians))
+		}
 	}
 	function describeArc(x, y, radius, startAngle, endAngle) {
 		var start = polarToCartesian(x, y, radius, endAngle)
@@ -43,6 +45,65 @@
 	}
 	bindings.xlink.once=1
 
+	function drawLine(node) {
+		var length = node.getTotalLength()
+		node.style.transition = node.style.WebkitTransition = "none"
+		node.style.strokeDasharray = length + " " + length
+		node.style.strokeDashoffset = length
+		// Trigger a layout so styles are calculated & the browser
+		// picks up the starting position before animating
+		node.getBoundingClientRect()
+		node.style.transition = node.style.WebkitTransition = "stroke-dashoffset 5s ease .5s"
+		node.style.strokeDashoffset = "0"
+	}
+	var arch1 = "a? ? 0 1 0 ? -?".split("?")
+	, arch2 = "a? ? 0 1 0 ? ?".split("?")
+	bindings.svgLine = function(points, opts) {
+		opts = opts || {}
+		var i = 0
+		, dataPoints = []
+		, radius = opts.radius || 0
+		, d = ["M" + (100 - radius), points[0]]
+		if (radius) {
+			d.push(arch1.join(radius), arch2.join(radius))
+		}
+
+		for (; ++i < points.length; ) {
+			d.push("C" + (i * 100 + 50 - radius), points[i-1] + "," + (i * 100 + 50 + radius), points[i] + "," + (i * 100 + 100 - radius), points[i])
+			//d.push("C" + (i * 100 + 50), points[i-1] + "," + (i * 100 + 50), points[i] + "," + (i * 100 + 100 - radius), points[i])
+			if (radius) {
+				d.push(arch1.join(radius), arch2.join(radius))
+			}
+			//dataPoints.push(El("circle[r=3][cx=" + (i * 100 + 100) + "][cy=" + points[i] + "]"))
+		}
+		this.setAttribute("d", d.join(" "))
+		drawLine(this)
+		this.parentNode.append(dataPoints)
+	}
+	var svgToLastActive
+	bindings.initChart = function() {
+		var node = this
+		node.on("mouseout", function(e) {
+			if (svgToLastActive && e.target == node) {
+				svgToLastActive.rmClass("is-active")
+				svgToLastActive = null
+			}
+		})
+	}
+	// riseOnHover
+	bindings.svgToLast = function() {
+		var node = this
+		node.on("mouseover", function() {
+			if (!svgToLastActive || node != node.parentNode.lastChild) {
+				if (svgToLastActive) svgToLastActive.rmClass("line-active")
+				node.after(node.parentNode.lastChild)
+				node.addClass("is-active")
+				svgToLastActive = node
+			}
+		})
+	}
+	bindings.svgToLast.once =
+	bindings.initChart.once = 1
 	function c(name) {
 		El.cache[name] = document.createElementNS(ns, name)
 		El.cache["svg|"+name] = document.createElementNS(ns, name)
