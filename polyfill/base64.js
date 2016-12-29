7b05c6ef7be894c3547bb1090cd179427a675e5d
Lauri Rooden <lauri@rooden.ee>
2016-12-29 19:54:42 +0200

    Include polyfills

---
+++ polyfill/base64.js
@@ -0,0 +1,35 @@
+
+
+
+!function(exports) {
+	if (!exports.atob) {
+		// abcdefghijklmnopqrstuvwxyz234567
+		// ybndrfg8ejkmcpqxot1uwisza345h769
+
+		for (var i = 64,
+			 ba = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(""),
+			 bm = {"=":0} ; bm[ba[--i]]=i;){};
+
+		// base64_encode
+		exports.btoa = function(s) {
+			for (var out=[],b,i=0,len=s.length;i<len;) {
+				b = s.charCodeAt(i++)<<16 | s.charCodeAt(i++)<<8 | s.charCodeAt(i++)
+				out.push(ba[b>>18&0x3f], ba[b>>12&0x3f], ba[b>>6&0x3f], ba[b&0x3f])
+			}
+			if (len%=3) out.splice(len-3, 2, len==1?"==":"=")
+			return out.join("")
+		}
+
+		// base64_decode
+		exports.atob = function(s) {
+			for (var out=[],b,i=0,len=s.length,s=s.split("");i<len;) {
+				b = bm[s[i++]]<<18 | bm[s[i++]]<<12 | bm[s[i++]]<<6 | bm[s[i++]]
+				out.push(b>>16 & 0xff, b>>8 & 0xff, b & 0xff)
+			}
+			if (s[len-1] == "=") out.length -= s[len-2] == "=" ? 2 : 1
+			return String.fromCharCode.apply(null, out)
+		}
+	}
+
+}(this)
+
---
+++ polyfill/index.js
@@ -0,0 +1,137 @@
+
+
+
+!function(exports) {
+	var a, b, c, O
+	, P = "prototype"
+	, F = Function
+	, esc = escape
+	, patched = exports._patched = []
+
+
+	function add(key, src) {
+		if (!O[key]) {
+			O[key] = F("a,b,c", "var P='" + P + "',o=Object[P].hasOwnProperty;" + src)
+			patched.push(key)
+		}
+	}
+
+	F.nop = function(){}
+
+	/*
+	* The HTML5 document.head DOM tree accessor
+	*/
+
+	//doc.head = doc.head || doc.getElementsByTagName("head")[0]
+
+	/*
+	* Function.prototype.bind from ECMAScript5
+	* Basic support: Chrome 7 Firefox 4 IE 9 Opera 11.60 Safari 5.1.4
+	*
+	* http://msdn.microsoft.com/en-us/library/s4esdbwz(v=vs.94).aspx
+	*/
+	O = F[P]
+	add("bind", "var t=this;b=[].slice.call(arguments,1);c=function(){return t.apply(this instanceof c?this:a,b.concat(b.slice.call(arguments)))};if(t[P])c[P]=t[P];return c")
+
+
+	// Object extensions
+	// -----------------
+
+	O = Object
+	add("create", "b=Function.nop;b[P]=a;a=new b;b[P]=null;return a")
+	add("keys", "c=[];for(b in a)o.call(a,b)&&c.push(b);return c")
+
+	// Object.assign ( target, source ) in ECMAScript 6
+	// Chrome 45, Firefox 34, IE Edge
+	add("assign", "for(P=1;b=arguments[P++];)for(c in b)if(o.call(b,c))a[c]=b[c];return a")
+
+	// Array extensions
+	// ----------------
+
+	O = Array
+	add("isArray", "return a instanceof Array")
+
+	O = O[P]
+	a = "var t=this,l=t.length,o=[],i=-1;"
+	c = "if(t[i]===a)return i;return -1"
+	add("indexOf",     a + "i+=b|0;while(++i<l)" + c)
+	add("lastIndexOf", a + "i=(b|0)||l;i>--l&&(i=l)||i<0&&(i+=l);++i;while(--i>-1)" + c)
+
+	b = a + "if(arguments.length<2)b=t"
+	c = "b=a.call(null,b,t[i],i,t);return b"
+	add("reduce",      b + "[++i];while(++i<l)" + c)
+	add("reduceRight", b + "[--l];i=l;while(i--)" + c)
+
+	b = a+"while(++i<l)if(i in t)"
+	add("forEach",     b + "a.call(b,t[i],i,t)")
+	add("every",       b + "if(!a.call(b,t[i],i,t))return!1;return!0")
+
+	c = ";return o"
+	add("map",         b + "o[i]=a.call(b,t[i],i,t)" + c)
+
+	b += "if(a.call(b,t[i],i,t))"
+	add("filter",      b + "o.push(t[i])" + c)
+	add("some",        b + "return!0;return!1")
+
+
+	O = String[P]
+	add("trim", "return this.replace(/^\\s+|\\s+$/g,'')")
+
+	// Chrome 24, FF 15, IE 10
+	O = exports.performance || (exports.performance = {})
+	add("now", (a = "return+new Date"))
+
+	O = Date
+	add("now", a)
+
+	/*
+	* `Date.prototype.format` is implemented in `date-format-lite` module.
+	*/
+
+	O = O[P]
+	add("toJSON", "return this.format('iso')")
+
+
+	if (!exports.JSON) {
+		patched.push("JSON")
+		exports.JSON = {
+			map: {"\b":"\\b","\f":"\\f","\n":"\\n","\r":"\\r","\t":"\\t",'"':'\\"',"\\":"\\\\"},
+			parse: F("t", "return Function('return('+t+')')()"),
+			stringify: F("o", ""
+				+ "var i,s=[],c=typeof o;"
+				+ "if(c=='string'){"
+					+ "for(i=o.length;c=o.charAt(--i);s[i]=JSON.map[c]||(c<' '?'\\\\u00'+((c=c.charCodeAt(0))|4)+(c%16).toString(16):c));"
+					+ "o='\"'+s.join('')+'\"'"
+				+ "}"
+				+ "if(o&&c=='object'){"
+					+ "if(typeof o.toJSON=='function')return'\"'+o.toJSON()+'\"';"
+					+ "if(Array.isArray(o)){"
+						+ "for(i=o.length;i--;s[i]=JSON.stringify(o[i]));"
+						+ "return'['+s.join()+']'"
+					+ "}"
+					+ "for(i in o)Object.prototype.hasOwnProperty.call(o,i)&&s.push(JSON.stringify(i)+':'+JSON.stringify(o[i]));"
+					+ "o='{'+s.join()+'}'"
+				+ "}"
+				+ "return o==null?'null':''+o"
+			)
+		}
+	}
+
+	// Ignore FF3 escape second non-standard argument
+	// https://bugzilla.mozilla.org/show_bug.cgi?id=666448
+	if (esc("a", 0) != "a") {
+		patched.push("escape")
+		exports.escape = function(s) {
+			return esc(s)
+		}
+	}
+
+	// Remove background image flickers on hover in IE6
+	//
+	// You could also use CSS
+	// html { filter: expression(document.execCommand("BackgroundImageCache", false, true)); }
+	eval("/*@cc_on try{document.execCommand('BackgroundImageCache',false,true)}catch(e){}@*/")
+}(this)
+
+
+
