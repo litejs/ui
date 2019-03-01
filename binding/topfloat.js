7ba2d82f0967026ff9bfc98c7604fe48ae2b21ab
Lauri Rooden <lauri@rooden.ee>
2019-03-01 20:57:00 +0200

    ui: Add binding topFloat

---
+++ binding/topfloat.js
@@ -0,0 +1,36 @@
+
+El.bindings.topFloat = function(el, offset) {
+	var h = el.offsetHeight + offset
+	, lastAbs = 0
+	, lastTop = 0
+	, toFix = 0
+
+	El.on(window, "scroll", function(e) {
+		var pos
+		, top = El.scrollTop()
+
+		// scroll up
+		if (top < lastTop) {
+			if (top <= toFix) {
+				el.style.position = "fixed"
+				el.style.top = toFix = 0
+			} else {
+				pos = lastTop - h
+				if (toFix < 0 || lastAbs < pos) {
+					el.style.position = "absolute"
+					el.style.top = (lastAbs = toFix = pos < 0 ? 0 : pos < lastAbs ? lastAbs : pos) + "px"
+				}
+			}
+		} else if (toFix === 0) {
+			el.style.position = "absolute"
+			el.style.top = lastTop + "px"
+			lastAbs = lastTop
+			toFix = -1
+		}
+		lastTop = top
+	})
+}
+
+El.bindings.topFloat.once = 1
+
+
