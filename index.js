fe1c8b34d709852139218eedc7fda45dae437876
Lauri Rooden <lauri@rooden.ee>
2021-04-16 11:24:09 +0300

    Release 21.4.0
    -- git fetch;git merge-base --is-ancestor @{upstream} HEAD
    
    -- rm -rf node_modules;npm install
    up to date, audited 1 package in 172ms
    
    found 0 vulnerabilities
    
    -- npm outdated
    
    -- npm outdated -g @litejs/cli uglify-js jshint nyc
    
    -- lj b --ver=21.4.0
    # Build @litejs/ui@21.4.0 with @litejs/cli@21.3.0
    WARN: test/min/0.js not commited?
    WARN: test/min/1.js not commited?
    WARN: test/min/2.js not commited?
    WARN: test/min/3.js not commited?
    
    -- lj test --brief
    # pass  8/8 [17/17] in 13 ms at 11:28:07

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-* @version  21.3.1
+* @version  21.4.0
 * @author   Lauri Rooden <lauri@rooden.ee>
 * @license  MIT License
 */
@@ -1052,7 +1052,6 @@
 /* litejs.com/MIT-LICENSE.txt */
 
 
-
 !function(window, document, Object, Event, protoStr) {
 	var styleNode
 	, BIND_ATTR = "data-bind"
@@ -1065,7 +1064,7 @@
 	, body = document.body
 	, root = document.documentElement
 	, txtAttr = El.T = "textContent" in body ? "textContent" : "innerText"
-	, templateRe = /^([ \t]*)(%?)((?:("|')(?:\\?.)*?\4|[-\w:.#[\]]=?)*)[ \t]*([>^;@|\\\/]|!?=|)(([\])}]?).*?([[({]?))$/gm
+	, templateRe = /([ \t]*)(%?)((?:("|')(?:\\?.)*?\4|[-\w:.#[\]]=?)*)[ \t]*([>^;@|\\\/]|!?=|)(([\])}]?).*?([[({]?))(?=\x1f+|\n+|$)/g
 	, renderRe = /[;\s]*(\w+)(?:\s*(:?):((?:(["'\/])(?:\\?.)*?\3|[^;])*))?/g
 	, selectorRe = /([.#:[])([-\w]+)(?:\((.+?)\)|([~^$*|]?)=(("|')(?:\\?.)*?\6|[-\w]+))?]?/g
 	, splitRe = /[,\s]+/
@@ -1527,7 +1526,10 @@
 				selector = null
 			}
 		}
-		if (delay > 0) return setTimeout(bindingOn, delay, el, events, selector, data, handler)
+		if (delay > 0) {
+			setTimeout(bindingOn, delay, el, events, selector, data, handler)
+			return
+		}
 		var fn = (
 			typeof handler == "string" ? function(e) {
 				var target = selector ? El.closest(e.target, selector) : el
@@ -1565,15 +1567,19 @@
 	}
 
 	function empty(el) {
-		for (var node; node = el.firstChild; ) {
-			kill(node)
-		}
+		for (var node; node = el.firstChild; kill(node));
 		return el
 	}
 
-	function kill(el) {
+	function kill(el, tr, delay) {
 		var id
 		if (el) {
+			if (delay) return setTimeout(kill, delay, el, tr)
+			if (tr) {
+				cls(el, tr, tr = "transitionend")
+				// transitionend fires for each property transitioned
+				if ("on" + tr in el) return addEvent(el, tr, kill.bind(el, el, el = null))
+			}
 			if (el._e) {
 				emitter.emit.call(el, "kill")
 				for (id in el._e) rmEvent(el, id)
@@ -1976,14 +1982,13 @@
 		if (map) {
 			kbMaps.unshift(map)
 			if (killEl) {
-				// addEvent(killEl, "kill", rmKb.bind(map, map))
 				emitter.on.call(killEl, "kill", rmKb.bind(map, map))
 			}
 		}
 	}
 	El.rmKb = rmKb
 	function rmKb(map) {
-		var i = kbMaps.indexOf(map||kbMaps[0])
+		var i = kbMaps.indexOf(map || kbMaps[0])
 		if (i > -1) kbMaps.splice(i, 1)
 	}
 
