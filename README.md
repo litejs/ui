3d79644aa26a1c443bfd9d2abf927328180c793d
Lauri Rooden <lauri@rooden.ee>
2021-06-16 12:39:11 +0300

    Release 21.6.0
    -- git fetch;git merge-base --is-ancestor @{upstream} HEAD
    
    -- rm -rf node_modules;npm install
    up to date, audited 1 package in 179ms
    
    found 0 vulnerabilities
    
    -- npm outdated
    
    -- npm outdated -g @litejs/cli uglify-js jshint nyc
    Package      Current  Wanted  Latest  Location                  Depended by
    @litejs/cli   21.4.0  21.3.0  21.3.0  node_modules/@litejs/cli  lib
    uglify-js     3.13.6  3.13.9  3.13.9  node_modules/uglify-js    lib
    
    -- ! git rev-parse -q --verify v21.6.0
    
    -- lj b
    # Build @litejs/ui@21.6.0 with @litejs/cli@21.4.0
    WARN: test/min/0.js not commited?
    WARN: test/min/1.js not commited?
    WARN: test/min/2.js not commited?
    WARN: test/min/3.js not commited?
    
    -- lj test --brief
    # pass  8/8 [17/17] in 19 ms at 12:39:11

--- README.md
+++ README.md
@@ -1,32 +1,39 @@
-[LiteJS]: https://www.litejs.com/
-[npm package]: https://npmjs.org/package/@litejs/ui
-[GitHub repo]: https://github.com/litejs/ui
-[LiteJS wiki]: https://github.com/litejs/litejs/wiki
+[LiteJS]: https://github.com/litejs/litejs
+[UI]: https://github.com/litejs/ui
+[CLI]: https://github.com/litejs/cli
+[Quick-Start]: https://github.com/litejs/litejs/wiki/Quick-Start
 
 [size]: https://packagephobia.now.sh/badge?p=@litejs/ui
 [size-src]: https://packagephobia.now.sh/result?p=@litejs/ui
 
+
+[LiteJS][] is a full-stack web framework in a tiny package.
+It is split into three main packages by designated install location:
+
+ - **[litejs][LiteJS]** - Server and core libraries installed as `dependencies`
+ - **[@litejs/ui][UI]** - UI engine installed as `devDependencies` and bundled into your app
+ - **[@litejs/cli][CLI]** - Command line tool installed as global package (or as `devDependencies`)
+
+> Build your first [LiteJS][] app with in 5 minute [Quick Start Guide][Quick-Start].
+
+
 LiteJS UI &ndash; [![size][size]][size-src]
 =========
 
-UI engine for LiteJS full-stack framework.
+UI engine for [LiteJS][] full-stack framework.
 
-
-## Get Started
-
-Build your first LiteJS app with in 5 minute [Quick Start Guide](https://github.com/litejs/litejs/wiki/Quick-Start).
-See [LiteJS wiki][] for more.
+> See [wiki][] for more.
 
 
 ## External links
 
-[GitHub repo][] |
-[npm package][]
+[GitHub repo](https://github.com/litejs/ui) |
+[npm package](https://npmjs.org/package/@litejs/ui)
 
 
 ## Licence
 
-Copyright (c) 2013-2020 Lauri Rooden &lt;lauri@rooden.ee&gt;  
+Copyright (c) 2013-2021 Lauri Rooden &lt;lauri@rooden.ee&gt;  
 [The MIT License](http://lauri.rooden.ee/mit-license.txt)
 
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-* @version  21.5.0
+* @version  21.6.0
 * @author   Lauri Rooden <lauri@rooden.ee>
 * @license  MIT License
 */
@@ -1074,7 +1074,7 @@
 	, splitRe = /[,\s]+/
 	, camelRe = /\-([a-z])/g
 	, bindings = El.bindings = {
-		attr: setAttr,
+		attr: El.attr = acceptMany(setAttr, getAttr),
 		cls: El.cls = acceptMany(cls),
 		css: El.css = acceptMany(function(el, key, val) {
 			el.style[key.replace(camelRe, camelFn)] = "" + val || ""
@@ -1205,10 +1205,6 @@
 
 	function camelFn(_, a) {
 		return a.toUpperCase()
-	}
-
-	El.attr = function(el, key, val) {
-		return arguments.length < 3 && key.constructor != Object ? getAttr(el, key) : setAttr(el, key, val)
 	}
 
 	function getAttr(el, key) {
@@ -1390,10 +1386,16 @@
 	function acceptMany(fn, getter) {
 		return function f(el, name, val, delay) {
 			if (el && name) {
-				if (delay > 0) return setTimeout(f, delay, el, name, val)
+				if (delay >= 0) {
+					if (delay > 0) setTimeout(f, delay, el, name, val)
+					else requestAnimationFrame(function() {
+						f(el, name, val)
+					})
+					return
+				}
 				if (name.constructor === Object) {
 					for (i in name) {
-						if (hasOwn.call(name, i)) f(el, i, name[i])
+						if (hasOwn.call(name, i)) f(el, i, name[i], val)
 					}
 					return
 				}
@@ -1578,7 +1580,7 @@
 	function kill(el, tr, delay) {
 		var id
 		if (el) {
-			if (delay) return setTimeout(kill, delay, el, tr)
+			if (delay > 0) return setTimeout(kill, delay, el, tr)
 			if (tr) {
 				cls(el, tr, tr = "transitionend")
 				// transitionend fires for each property transitioned
@@ -1638,14 +1640,14 @@
 
 			fn = "data b s B r->data&&(" + bind.replace(renderRe, function(match, name, op, args) {
 				scope._m[i] = match
-				var fn = bindings[name]
+				match = bindings[name]
 				return (
-					(op == ":" || fn && hasOwn.call(fn, "once")) ?
+					(op == ":" || match && hasOwn.call(match, "once")) ?
 					"s(this,B,data._t=data._t.replace(data._m[" + (i++)+ "],''))||" :
 					""
 				) + (
-					fn ?
-					"b['" + name + "'].call(data,this" + (fn.raw ? ",'" + args + "'" : args ? "," + args : "") :
+					match ?
+					"b['" + name + "'].call(data,this" + (match.raw ? ",'" + args + "'" : args ? "," + args : "") :
 					"s(this,'" + name + "'," + args
 				) + ")||"
 			}) + "r)"
@@ -1708,10 +1710,10 @@
 	}
 
 	wrapProto.cloneNode = function(deep) {
-		var clone = new ElWrap(this, deep)
-		clone._ca = this._ca
-		//clone._cb = this._cb
-		return clone
+		deep = new ElWrap(this, deep)
+		deep._ca = this._ca
+		//deep._cb = this._cb
+		return deep
 	}
 
 	El.append = append
@@ -1992,8 +1994,8 @@
 	}
 	El.rmKb = rmKb
 	function rmKb(map) {
-		var i = kbMaps.indexOf(map || kbMaps[0])
-		if (i > -1) kbMaps.splice(i, 1)
+		map = kbMaps.indexOf(map || kbMaps[0])
+		if (map > -1) kbMaps.splice(map, 1)
 	}
 
 	addEvent(document, "keydown", kbDown)
@@ -2045,276 +2047,3 @@
 	addEvent(window, "load", setBreakpointsRated)
 	/**/
 }(window, document, Object, Event, "prototype")
-/* litejs.com/MIT-LICENSE.txt */
-
-
-
-!function(bindings) {
-	var hasOwn = Object.prototype.hasOwnProperty
-	, slice = Array.prototype.slice
-
-	fixReadonlyCheckbox.once =
-	bindingEvery.once =
-	emitForm.once =
-	bindingFn.once =
-	bindingsEach.raw = bindingsEach.once =
-	true
-
-	bindings.every = bindingEvery
-	function bindingEvery(el, list, attrName) {
-		var len = 0
-		, data = this
-		, parent = el.parentNode
-		, comm = document.createComment("every " + (list.name || list.length))
-		, nodes = []
-
-		parent.replaceChild(comm, el)
-
-		if (list) {
-			if (typeof list === "string") {
-				data.model.on("change:" + list, render)
-				El.on(parent, "kill", function() {
-					data.model.off("change:" + list, render)
-				})
-				render()
-			} else if (list.eachLive) {
-				list.eachLive(add, remove, list)
-				El.on(parent, "kill", function() {
-					list.off("add", add, list).off("remove", remove, list)
-				})
-			} else {
-				comm.render = render
-				render()
-			}
-		}
-		return true
-
-		function render() {
-			for (; len; len--) {
-				remove(len)
-			}
-			_list = typeof list === "string" ? data.model.get(list, []) : list
-			if (typeof _list === "string") _list.split(",")
-			Object.each(_list, add, (
-				_list.constructor === Object ? Object.keys(_list) : _list
-			))
-		}
-
-		function add(item, i) {
-			len++
-			var up
-			, clone = el.cloneNode(true)
-			, scope = El.scope(clone, data)
-			, before = nodes[i] || comm
-			if (!before.parentNode) return
-			nodes.splice(i, 0, clone)
-			scope.i = i
-			scope._scope = scope
-			scope.len = this.length
-			scope[attrName || "item"] = item
-			El.append(before.parentNode, clone, before)
-			El.render(clone, scope)
-			if (typeof item.on === "function") {
-				item.on("change", up = El.render.bind(clone, clone))
-				El.on(clone, "kill", function() {
-					item.off("change", up)
-				})
-			}
-		}
-
-		function remove(item, i) {
-			El.kill(nodes.splice(i, 1)[0])
-		}
-	}
-
-	bindings.fixReadonlyCheckbox = fixReadonlyCheckbox
-	function fixReadonlyCheckbox(el) {
-		El.on(el, "click pointerdown", function(e) {
-			if ((this.firstChild || this).readOnly) {
-				return Event.stop(e)
-			}
-		})
-	}
-
-	bindings.fn = bindingFn
-	function bindingFn(el, fn) {
-		return fn.apply(el, slice.call(arguments, 3))
-	}
-
-	bindings["if"] = bindingsIf
-	function bindingsIf(el, enabled) {
-		var parent = el.parentNode
-		, scope = this
-		if (enabled) {
-			parent || el._ifComm && el._ifComm.parentNode.replaceChild(el, el._ifComm)
-		} else {
-			if (parent) {
-				if (!el._ifComm) {
-					El.on(el, "kill", El.kill.bind(el, el._ifComm = document.createComment("if")))
-					el._ifComm.render = function() {
-						El.render(el, scope)
-					}
-				}
-				parent.replaceChild(el._ifComm, el)
-			}
-			return true
-		}
-	}
-
-	bindings.is = function bindingIs(node, model, path, list, state) {
-		var match
-		, scope = this
-		if (typeof model === "string") {
-			state = list
-			list = path
-			path = model
-			model = scope.model
-		}
-		if (model && path) {
-			match = i18n.pick(state != null ? state : model.get(path), list)
-			El.cls(node, scope["_is-" + path], 0)
-			El.cls(node, scope["_is-" + path] = match && "is-" + match)
-		}
-	}
-
-	bindings.emitForm = emitForm
-	function emitForm(el, ev, a1, a2, a3, a4) {
-		El.on(el, "submit", function(e) {
-			var data = El.val(el)
-			View.emit(ev, e, data, a1, a2, a3, a4)
-			return Event.stop(e)
-		})
-	}
-
-	function getChilds(node) {
-		var child
-		, childs = node._childs
-		if (!childs) {
-			for (node._childs = childs = []; child = node.firstChild;) {
-				childs.push(child);
-				node.removeChild(child)
-			}
-		}
-		return childs
-	}
-
-	bindings.each = bindingsEach
-
-	function bindingsEach(el, expr) {
-		var node = el
-		, child = getChilds(node)[0]
-		, match = /^\s*(\w+) in (\w*)(.*)/.exec(expr)
-		, fn = "with(data){var out=[],loop={i:0,offset:0},_1,_2=" + match[2]
-		+ match[3].replace(/ (limit|offset):\s*(\d+)/ig, ";loop.$1=$2")
-		+ ";if(_2)for(_1 in _2)if(hasOwn.call(_2,_1)&&!(loop.offset&&loop.offset--)){"
-		+     "loop.i++;"
-		+     "if(loop.limit&&loop.i-loop.offset>loop.limit)break;"
-		+     "var clone=el.cloneNode(true)"
-		+     ",scope=El.scope(clone,data);"
-		+     "scope.loopKey=loop.key=_1;"
-		+     "scope.loop=loop;"
-		+     "scope." + match[1] + "=_2[_1];"
-		+     "out.push(clone);"
-		+ "};return out}"
-
-		var childs = Function("hasOwn,el,data", fn)(hasOwn, child, this)
-
-		El.append(El.empty(node), childs)
-		El.render(node)
-		return node
-	}
-
-	bindings.focus = function(el) {
-		el.focus()
-	}
-
-	bindings.href = function(el, url) {
-		if (url) {
-			var chr = url.charAt(0)
-			El.attr(el, "href", chr === "+" || chr === "%" ? "#" + View.expand(url) : url)
-		}
-	}
-}(El.bindings)
-/* litejs.com/MIT-LICENSE.txt */
-
-
-
-El.bindings.list = function(node, list, extra, val) {
-	var child = node._child
-	, data = this
-	, extraLen = 0
-
-	if (!child) {
-		child = node._child = node.removeChild(node.firstChild)
-	}
-
-	if (!list || node._list == list) return
-
-	if (node._list) clear()
-
-	node._list = data.list = list
-
-	El.on(node, "kill", clear)
-
-	El.cls(node, "loading")
-
-	if (extra) {
-		extra.each(clone)
-		extraLen = extra.length
-	}
-
-	list
-	.each(clone)
-	.on("add", clone).on("remove", remove)
-	.on("startLoading", function(){
-		El.cls(node, "loading")
-	})
-	.on("endLoading", function(){
-		El.cls(node, "loading", 0)
-	})
-	.then(function() {
-		if (val !== void 0 && El.val(node) !== val) {
-			if (!this.get(val)) {
-				clone({id:val,name:val}, extraLen)
-				extraLen++
-			}
-			El.val(node, val)
-		}
-		El.cls(node, "loading", 0)
-	})
-
-	// Do not render childs when list initialized
-	return true
-
-	function clone(item, pos) {
-		var clone = child.cloneNode(true)
-		, scope = El.scope(clone, data)
-		scope.item = item.data || item || {id:item,name:item}
-		scope.model = item
-		scope.pos = pos
-		function up() {
-			El.render(clone, scope)
-		}
-		if (item.on) {
-			item.on("change", up)
-			El.on(clone, "kill", function(){
-				item.off("change", up)
-			})
-		}
-		El.append(node, clone, extraLen + pos)
-		return El.render(clone, scope)
-	}
-
-	function remove(item, pos) {
-		El.kill(node.childNodes[extraLen + pos])
-	}
-
-	function clear() {
-		var list = node._list
-		El.empty(node)
-		if (list) {
-			node._list = null
-			list.off("add", clone).off("remove", remove)
-		}
-	}
-}
