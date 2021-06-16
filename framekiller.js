c8c706467c9013bca031118ea89a1302adad3376
Lauri Rooden <lauri@rooden.ee>
2021-06-16 12:19:49 +0300

    Add framekiller.js

---
+++ framekiller.js
@@ -0,0 +1,8 @@
+
+// Clickjacking defense
+//  - If JavaScript is disabled, the site will not display at all
+//  - Top-level navigation requires a user gesture
+//    https://github.com/WICG/interventions/issues/16
+if (self !== top) throw "Framed: " + document.referrer
+
+
