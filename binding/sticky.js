3ce2874e1ee9dc1197495db4b2483466e131a727
Lauri Rooden <lauri@rooden.ee>
2022-05-25 21:16:05 +0300

    Add binding/sticky

---
+++ binding/sticky.js
@@ -0,0 +1,16 @@
+
+// .sticky { position: sticky; top: -1px; }
+// .sticky.is-stuck { color: red; }
+
+El.bindings.sticky = function sticky(el) {
+	;(sticky._ob || (sticky._ob = new IntersectionObserver(function(entries) {
+		entries.forEach(function(entry) {
+			El.cls(entry.target, "is-stuck", entry.intersectionRatio < 1)
+		})
+	}, { threshold: 1 }))).observe(el)
+	El.cls(el, "sticky")
+}
+
+El.bindings.sticky.once = 1
+
+
