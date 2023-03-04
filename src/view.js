afa9b7867b93cde5f370e1e63c8576f4665ca40f
Lauri Rooden <lauri@rooden.ee>
2023-03-04 16:21:44 +0200

    view: Fix syncResume

--- src/view.js
+++ src/view.js
@@ -114,7 +114,7 @@
 			if (view !== close) emit(view, "change", close)
 
 			for (tmp in params) if (tmp.charAt(0) !== "_") {
-				if ((syncResume = hasOwn.call(paramCb, tmp)) && paramCb[tmp] || paramCb["*"]) {
+				if ((syncResume = hasOwn.call(paramCb, tmp) && paramCb[tmp] || paramCb["*"])) {
 					syncResume.call(view, params[tmp], tmp, params)
 					syncResume = null
 				}
