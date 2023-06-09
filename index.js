996d04163abd051081fc50bae630168b59d9b5ff
Lauri Rooden <lauri@rooden.ee>
2023-06-09 15:30:57 +0300

    Release v24.0.0-rc.1
    -- npm outdated
    
    -- ! git rev-parse -q --verify v24.0.0-rc.1
    
    -- lj build
    
    -- lj test --brief test/index.js
    # pass  21/21 [1120/1120] in 41 ms at 15:30:51

--- index.js
+++ index.js
@@ -103,137 +103,18 @@
 
 
 
+/* global El, xhr */
 !function(window, document, history, location) {
-	var cb, base, lastRoute, iframe, tick, last
+	var histCb, histBase, histRoute, iframe, iframeTick, iframeUrl
 	, cleanRe = /^[#\/\!]+|[\s\/]+$/g
 
-	// The JScript engine used in IE doesn't recognize vertical tabulation character
+	// JScript engine in IE8 and below does not recognize vertical tabulation character `\v`.
 	// http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html
-	// oldIE = "\v" == "v"
 	//
-	// The documentMode is an IE only property, supported in IE8+.
-	//
-	// Starting in Internet Explorer 9 standards mode, Internet Explorer 10 standards mode,
-	// and win8_appname_long apps, you cannot identify the browser as Internet Explorer
-	// by testing for the equivalence of the vertical tab (\v) and the "v".
-	// In earlier versions, the expression "\v" === "v" returns true.
-	// In Internet Explorer 9 standards mode, Internet Explorer 10 standards mode,
-	// and win8_appname_long apps, the expression returns false.
+	// The documentMode is an IE only property, supported in IE8 and up.
 	, ie67 = !+"\v1" && (document.documentMode | 0) < 8 // jshint ignore:line
 
-	function getUrl(_loc) {
-		return (
-			/*** PUSH ***/
-			base ? location.pathname.slice(base.length) :
-			/**/
-			// bug in Firefox where location.hash is decoded
-			// bug in Safari where location.pathname is decoded
-
-			// var hash = location.href.split('#')[1] || '';
-			// https://bugs.webkit.org/show_bug.cgi?id=30225
-			// https://github.com/documentcloud/backbone/pull/967
-			(_loc || location).href.split("#")[1] || ""
-		).replace(cleanRe, "")
-	}
-
-	function setUrl(url, replace) {
-		/*** PUSH ***/
-		if (base) {
-			history[replace ? "replaceState" : "pushState"](null, null, base + url)
-		} else {
-		/**/
-			location[replace ? "replace" : "assign"]("#" + url)
-			// Opening and closing the iframe tricks IE7 and earlier
-			// to push a history entry on hash-tag change.
-			if (iframe && getUrl() !== getUrl(iframe.location) ) {
-				iframe.location[replace ? "replace" : iframe.document.open().close(), "assign"]("#" + url)
-			}
-		/*** PUSH ***/
-		}
-		/**/
-		return checkUrl()
-	}
-
-	function checkUrl() {
-		if (lastRoute != (lastRoute = getUrl())) {
-			if (cb) cb(lastRoute)
-			return true
-		}
-	}
-
-	history.getUrl = getUrl
-	history.setUrl = setUrl
-
-	history.start = function(_cb) {
-		cb = _cb
-		/*** PUSH ***/
-		// Chrome5, Firefox4, IE10, Safari5, Opera11.50
-		var url
-		, _base = document.documentElement.getElementsByTagName("base")[0]
-		if (_base) _base = _base.href.replace(/.*:\/\/[^/]*|[^\/]*$/g, "")
-		if (_base && !history.pushState) {
-			url = location.pathname.slice(_base.length)
-			if (url) {
-				location.replace(_base + "#" + url)
-			}
-		}
-		if (_base && history.pushState) {
-			base = _base
-
-			url = location.href.split("#")[1]
-			if (url && !getUrl()) {
-				setUrl(url, 1)
-			}
-
-			// Chrome and Safari emit a popstate event on page load, Firefox doesn't.
-			// Firing popstate after onload is as designed.
-			//
-			// See the discussion on https://bugs.webkit.org/show_bug.cgi?id=41372,
-			// https://code.google.com/p/chromium/issues/detail?id=63040
-			// and the change to the HTML5 spec that was made:
-			// http://html5.org/tools/web-apps-tracker?from=5345&to=5346.
-			window.onpopstate = checkUrl
-		} else
-		/**/
-			if ("onhashchange" in window && !ie67) {
-			// There are onhashchange in IE7 but its not get emitted
-			//
-			// Basic support:
-			// Chrome 5.0, Firefox 3.6, IE 8, Opera 10.6, Safari 5.0
-			window.onhashchange = checkUrl
-		} else {
-			if (ie67 && !iframe) {
-				// IE<9 encounters the Mixed Content warning when the URI javascript: is used.
-				// IE5/6 additionally encounters the Mixed Content warning when the URI about:blank is used.
-				// src="//:"
-				iframe = document.body.appendChild(document.createElement("<iframe tabindex=-1 style=display:none>")).contentWindow
-			}
-			clearInterval(tick)
-			tick = setInterval(function(){
-				var cur = getUrl()
-				if (iframe && last === cur) cur = getUrl(iframe.location)
-				if (last !== cur) {
-					last = cur
-					if (iframe) setUrl(cur)
-					else checkUrl()
-				}
-			}, 60)
-		}
-		checkUrl()
-	}
-}(this, document, history, location) // jshint ignore:line
-
-
-
-
-
-/* litejs.com/MIT-LICENSE.txt */
-
-
-
-/* global El, xhr */
-!function(exports) {
-	var fn, lastView, lastStr, lastUrl, syncResume
+	, fn, lastView, lastStr, lastUrl, syncResume
 	, body = document.body
 	, isArray = Array.isArray
 	, capture = 1
@@ -246,13 +127,13 @@
 	, escapeRe = /[.*+?^=!:${}()|\[\]\/\\]/g
 	, parseRe = /\{([\w%.]+?)\}|.[^{\\]*?/g
 	, defaults = {
-		base: "view/",
+		base: "",
 		home: "home",
 		root: body
 	}
 
-	exports.View = View
-	exports.LiteJS = LiteJS
+	window.View = View
+	window.LiteJS = LiteJS
 
 
 	function LiteJS(opts) {
@@ -491,7 +372,110 @@
 		)
 	}
 
-}(this) // jshint ignore:line
+	function getUrl(_loc) {
+		return (
+			/*** pushState ***/
+			histBase ? location.pathname.slice(histBase.length) :
+			/**/
+			// bug in Firefox where location.hash is decoded
+			// bug in Safari where location.pathname is decoded
+
+			// var hash = location.href.split('#')[1] || '';
+			// https://bugs.webkit.org/show_bug.cgi?id=30225
+			// https://github.com/documentcloud/backbone/pull/967
+			(_loc || location).href.split("#")[1] || ""
+		).replace(cleanRe, "")
+	}
+
+	function setUrl(url, replace) {
+		/*** pushState ***/
+		if (histBase) {
+			history[replace ? "replaceState" : "pushState"](null, null, histBase + url)
+		} else {
+		/**/
+			location[replace ? "replace" : "assign"]("#" + url)
+			// Opening and closing the iframe tricks IE7 and earlier
+			// to push a history entry on hash-tag change.
+			if (iframe && getUrl() !== getUrl(iframe.location) ) {
+				iframe.location[replace ? "replace" : iframe.document.open().close(), "assign"]("#" + url)
+			}
+		/*** pushState ***/
+		}
+		/**/
+		return checkUrl()
+	}
+
+	function checkUrl() {
+		if (histRoute != (histRoute = getUrl())) {
+			if (histCb) histCb(histRoute)
+			return true
+		}
+	}
+
+	history.getUrl = getUrl
+	history.setUrl = setUrl
+
+	LiteJS.start = function(cb) {
+		histCb = cb
+		/*** pushState ***/
+		// Chrome5, Firefox4, IE10, Safari5, Opera11.50
+		var url
+		, _base = document.documentElement.getElementsByTagName("base")[0]
+		if (_base) _base = _base.href.replace(/.*:\/\/[^/]*|[^\/]*$/g, "")
+		if (_base && !history.pushState) {
+			url = location.pathname.slice(_base.length)
+			if (url) {
+				location.replace(_base + "#" + url)
+			}
+		}
+		if (_base && history.pushState) {
+			histBase = _base
+
+			url = location.href.split("#")[1]
+			if (url && !getUrl()) {
+				setUrl(url, 1)
+			}
+
+			// Chrome and Safari emit a popstate event on page load, Firefox doesn't.
+			// Firing popstate after onload is as designed.
+			//
+			// See the discussion on https://bugs.webkit.org/show_bug.cgi?id=41372,
+			// https://code.google.com/p/chromium/issues/detail?id=63040
+			// and the change to the HTML5 spec that was made:
+			// http://html5.org/tools/web-apps-tracker?from=5345&to=5346.
+			window.onpopstate = checkUrl
+		} else
+		/**/
+			if ("onhashchange" in window && !ie67) {
+			// There are onhashchange in IE7 but its not get emitted
+			//
+			// Basic support:
+			// Chrome 5.0, Firefox 3.6, IE 8, Opera 10.6, Safari 5.0
+			window.onhashchange = checkUrl
+		} else {
+			if (ie67 && !iframe) {
+				// IE<9 encounters the Mixed Content warning when the URI javascript: is used.
+				// IE5/6 additionally encounters the Mixed Content warning when the URI about:blank is used.
+				// src="//:"
+				iframe = document.body.appendChild(document.createElement("<iframe tabindex=-1 style=display:none>")).contentWindow
+			}
+			clearInterval(iframeTick)
+			iframeTick = setInterval(function(){
+				var cur = getUrl()
+				if (iframe && iframeUrl === cur) cur = getUrl(iframe.location)
+				if (iframeUrl !== cur) {
+					iframeUrl = cur
+					if (iframe) setUrl(cur)
+					else checkUrl()
+				}
+			}, 60)
+		}
+
+		xhr.load(View.$$("script[type='litejs/view']", body).map(function(el) {
+			return el.src
+		}), checkUrl)
+	}
+}(this, document, history, location) // jshint ignore:line
 
 
 
