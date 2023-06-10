1f3622399df81cae6c77cc96f3474123f83a4d63
Lauri Rooden <lauri@rooden.ee>
2023-06-10 13:49:47 +0300

    Merge view.js and event.js

--- src/view.js
+++ src/view.js
@@ -1,10 +1,102 @@
 
 /* litejs.com/MIT-LICENSE.txt */
 
-
-
 /* global El, xhr */
+
 !function(window, document, history, location) {
+	var empty = []
+	, Event = window.Event || window
+
+	Event.Emitter = EventEmitter
+	Event.asEmitter = asEmitter
+
+	function EventEmitter() {}
+
+	function asEmitter(obj) {
+		obj.on = on
+		obj.off = off
+		obj.one = one
+		obj.emit = emit
+		obj.listen = listen
+		obj.unlisten = unlisten
+	}
+	asEmitter(EventEmitter.prototype)
+
+	function on(type, fn, scope, _origin) {
+		var emitter = this === window ? empty : this
+		, events = emitter._e || (emitter._e = Object.create(null))
+		if (type && fn) {
+			if (typeof fn === "string") fn = emit.bind(emitter, fn)
+			emit.call(emitter, "newListener", type, fn, scope, _origin)
+			;(events[type] || (events[type] = [])).unshift(scope, _origin, fn)
+		}
+		return this
+	}
+
+	function off(type, fn, scope) {
+		var i, args
+		, emitter = this === window ? empty : this
+		, events = emitter._e && emitter._e[type]
+		if (events) {
+			for (i = events.length - 2; i > 0; i -= 3) {
+				if ((events[i + 1] === fn || events[i] === fn) && events[i - 1] == scope) {
+					args = events.splice(i - 1, 3)
+					emit.call(emitter, "removeListener", type, args[2], args[0], args[1])
+					if (fn) break
+				}
+			}
+		}
+		return this
+	}
+
+	function one(type, fn, scope) {
+		var emitter = this === window ? empty : this
+		function remove() {
+			off.call(emitter, type, fn, scope)
+			off.call(emitter, type, remove, scope)
+		}
+		on.call(emitter, type, remove, scope)
+		on.call(emitter, type, fn, scope)
+		return this
+	}
+
+	// emitNext
+	// emitLate
+
+	function emit(type) {
+		var args, i
+		, emitter = this === window ? empty : this
+		, _e = emitter._e
+		, arr = _e ? (_e[type] || empty).concat(_e["*"] || empty) : empty
+		if ((_e = arr.length)) {
+			for (i = _e - 1, args = arr.slice.call(arguments, 1); i > 1; i -= 3) {
+				if (arr[i]) arr[i].apply(arr[i - 2] || emitter, args)
+			}
+		}
+		return _e / 3
+	}
+
+	function listen(emitter, ev, fn, scope, _origin) {
+		if (emitter) {
+			on.call(emitter, ev, fn, scope)
+			;(this._l || (this._l = [])).push([emitter, ev, fn, scope, _origin])
+		}
+		return this
+	}
+
+	function unlisten(key) {
+		var a, i
+		, listening = this._l
+		if (listening) for (i = listening.length; i--; ) {
+			a = listening[i]
+			if (key === "*" || a.indexOf(key) > -1) {
+				listening.splice(i, 1)
+				off.call(a[0], a[1], a[2], a[3])
+			}
+		}
+		return this
+	}
+
 	var histCb, histBase, histRoute, iframe, iframeTick, iframeUrl
 	, cleanRe = /^[#\/\!]+|[\s\/]+$/g
 
@@ -97,10 +189,10 @@
 			, close = view.isOpen && view
 
 			View.route = view.route
-			emit(view, "init")
+			viewEmit(view, "init")
 
 			for (; tmp; tmp = parent) {
-				emit(syncResume = params._v = tmp, "ping", params, View)
+				viewEmit(syncResume = params._v = tmp, "ping", params, View)
 				syncResume = null
 				if (lastParams !== params) return
 				if ((parent = tmp.parent)) {
@@ -128,7 +220,7 @@
 				}
 			}
 
-			if (view !== close) emit(view, "change", close)
+			if (view !== close) viewEmit(view, "change", close)
 
 			for (tmp in params) if (tmp.charAt(0) !== "_") {
 				if ((syncResume = hasOwn.call(paramCb, tmp) && paramCb[tmp] || paramCb["*"])) {
@@ -168,8 +260,8 @@
 			)
 			El.append(tmp, view.isOpen)
 			El.render(view.isOpen)
-			emit(parent, "openChild", view, close)
-			emit(view, "open", params)
+			viewEmit(parent, "openChild", view, close)
+			viewEmit(view, "open", params)
 			if (view.kb) El.addKb(view.kb)
 			close = null
 		}
@@ -177,29 +269,29 @@
 			bubbleDown(params, close)
 		}
 		if ((lastView === view)) {
-			emit(view, "show", params)
+			viewEmit(view, "show", params)
 			blur()
 		}
 	}
 
 	function closeView(view, open) {
 		if (view && view.isOpen) {
-			emit(view.parent, "closeChild", view, open)
+			viewEmit(view.parent, "closeChild", view, open)
 			closeView(view.child)
 			El.kill(view.isOpen)
 			view.isOpen = null
 			if (view.kb) El.rmKb(view.kb)
-			emit(view, "close")
+			viewEmit(view, "close")
 		}
 	}
 
-	function emit(view, event, a, b) {
+	function viewEmit(view, event, a, b) {
 		view.emit(event, a, b)
 		View.emit(event, view, a, b)
 	}
 
-	Event.asEmitter(View)
-	Event.asEmitter(View.prototype)
+	asEmitter(View)
+	asEmitter(View.prototype)
 
 	View.get = get
 	function get(url, params) {
