9f3a7f7b340336da41c8089bbd7f2a3aa958d1fe
Lauri Rooden <lauri@rooden.ee>
2023-07-07 11:06:14 +0300

    Remove framekiller.js

--- framekiller.js
+++
@@ -1,9 +0,0 @@
-/* globals self, top, document */
-
-// Clickjacking defense
-//  - If JavaScript is disabled, the site will not display at all
-//  - Top-level navigation requires a user gesture
-//    https://github.com/WICG/interventions/issues/16
-if (self !== top) throw "Framed: " + document.referrer
-
-
